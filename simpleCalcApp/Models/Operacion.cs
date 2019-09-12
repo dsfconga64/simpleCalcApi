d96860ef7efda455c3e0259f834a337a9739bd8a
dsf95 <davidsfrini3@gmail.com>
2019-09-12 12:18:29 -0600

    added logic to Operacion model specific to the Sumar method

--- simpleCalcApp/Models/Operacion.cs
+++ simpleCalcApp/Models/Operacion.cs
@@ -7,16 +7,64 @@ namespace simpleCalcApp.Models
 {
     public class Operacion
     {
+        public static Operacion op = new Operacion();
         //Comment cambio github
         public string tipOperacion { get; set; }
         public double valor1{ get; set; }
         public double valor2 { get; set; }
         public double  resultado{ get; set; }
 
-        public static  Operacion procesar (Operacion poperacion)
+        public Operacion()
         {
-            return null;
+        }
+
+        public Operacion(string ptipo, double pvalor1, double pvalor2, double presultado)
+        {
+            this.tipOperacion = ptipo;
+            this.valor1 = pvalor1;
+            this.valor2 = pvalor2;
+            this.resultado = presultado;
         }
         
+        public static  Operacion procesar (Operacion poperacion)
+        {
+            switch (poperacion.tipOperacion)
+            {
+                case "Suma":
+                    return Sumar(poperacion);
+                    //break;
+                case "Resta":
+                    return Restar(poperacion);
+                    //break;
+                case "Division":
+                    return Dividir(poperacion);
+                    //break;
+                case "Multiplicacion":
+                    return Multiplicar(poperacion);
+                    //break;
+            }
+            return null;
+        }
+
+        private static Operacion Multiplicar(Operacion poperacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Operacion Dividir(Operacion poperacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Operacion Restar(Operacion poperacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Operacion Sumar(Operacion poperacion)
+        {
+            return  op = new Operacion(poperacion.tipOperacion,poperacion.valor1, poperacion.valor2, poperacion.valor1+poperacion.valor2);
+ 
+        }
     }
 }
