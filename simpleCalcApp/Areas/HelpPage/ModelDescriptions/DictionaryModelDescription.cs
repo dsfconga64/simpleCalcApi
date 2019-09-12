18e05b7a3a61d0b4df6e0696f797c970a6a4dde1
dsolanof <dsolanof@ucenfotec.ac.cr>
2019-09-12 00:04:43 -0600

    Add project files.

---
+++ simpleCalcApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace simpleCalcApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ simpleCalcApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace simpleCalcApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
