7c176422f53c3a72803f99bc6a6cf08edaf7b78d
dsolanof <dsolanof@ucenfotec.ac.cr>
2019-09-12 00:14:14 -0600

    added operacion class

---
+++ simpleCalcApp/Models/Operacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simpleCalcApp.Models
+{
+    public class Operacion
+    {
+        public string tipOperacion { get; set; }
+        public double valor1{ get; set; }
+        public double valor2 { get; set; }
+        public double  resultado{ get; set; }
+        
+    }
+}
